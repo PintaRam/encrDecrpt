34e3bff334d0181ff82cd0df714e1974bdde0eb8
PINTA RAM <pintu.borana20@gmail.com>
2024-03-17 20:22:55 +0530

    Initial commit

--- app/src/main/java/com/encrdecrpt/MainActivity.java
+++ app/src/main/java/com/encrdecrpt/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "tjisddsd", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "sd", Toast.LENGTH_SHORT).show();
     }
 }
