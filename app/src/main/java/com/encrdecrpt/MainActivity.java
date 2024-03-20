206464f4bb1e2cc61d5762619b9ca6eb4bbcf2ed
PINTA RAM <pintu.borana20@gmail.com>
2024-03-20 10:24:25 +0530

    Initial commit

--- app/src/main/java/com/encrdecrpt/MainActivity.java
+++ app/src/main/java/com/encrdecrpt/MainActivity.java
@@ -2,7 +2,11 @@ package com.encrdecrpt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +17,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "king", Toast.LENGTH_SHORT).show();
         String  str;
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+        Button btn1 = findViewById(R.id.button2);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i  = new Intent(MainActivity.this , login.class);
+                startActivity(i);
+            }
+        });
+
 
     }
 }
